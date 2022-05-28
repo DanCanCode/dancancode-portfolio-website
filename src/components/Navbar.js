99df740209bb11adfa52192ed07f84329e9dbf2a
Daniel Espinal <dancancode@protonmail.com>
2022-05-28 14:46:04 -0400

    navbar setup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,57 @@
 import React from "react";
 import { Link as ReactLink } from "react-router-dom";
-import { Box, Text, Link } from "@chakra-ui/react";
+import {
+  Flex,
+  Box,
+  Text,
+  Link,
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  useDisclosure,
+  Icon,
+} from "@chakra-ui/react";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
-    <Box as="nav" width="100%" backgroundColor="#000000">
-      <Text margin={0} color="#ffffff">
-        Hello
+    <Flex
+      as="nav"
+      paddingY={15}
+      paddingX={30}
+      backgroundColor="rgba(0, 0, 0, 0.1)"
+      flexDirection="row"
+      justifyContent="space-between"
+      alignItems="center"
+    >
+      <Text margin={0} fontWeight="bold" fontSize={28}>
+        Daniel Espinal
       </Text>
-      <Link color="#ffffff" as={ReactLink} to="/projects">
-        Projects
-      </Link>
-    </Box>
+      <Box
+        as="button"
+        borderWidth={0}
+        backgroundColor="rgba(255, 255, 255, 0)"
+        color="#ffffff"
+        padding={0}
+        _hover={{
+          color: "#808080",
+        }}
+      >
+        <Icon
+          as={GiHamburgerMenu}
+          boxSize={36}
+          _active={{
+            transform: "scale(0.9)",
+          }}
+        />
+      </Box>
+    </Flex>
   );
 };
 
