307b0f096d589c208c07c980d53d2a7263074282
Daniel Espinal <dancancode@protonmail.com>
2022-05-28 19:49:38 -0400

    sidebar adjustments

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,6 +17,9 @@ import {
   Button,
 } from "@chakra-ui/react";
 import { FiMenu } from "react-icons/fi";
+import { IoPersonSharp, IoHomeSharp } from "react-icons/io5";
+import { IoIosConstruct } from "react-icons/io";
+import { BsLinkedin, BsGithub } from "react-icons/bs";
 
 const Navbar = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -50,18 +53,82 @@ const Navbar = () => {
         <DrawerOverlay />
         <DrawerContent backgroundColor="#808080">
           <DrawerCloseButton />
-          <DrawerHeader>{`Home drawer contents`}</DrawerHeader>
+
           <DrawerBody>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Consequat nisl vel pretium lectus quam id. Semper quis lectus
-              nulla at volutpat diam ut venenatis. Dolor morbi non arcu risus
-              quis varius quam quisque. Massa ultricies mi quis hendrerit dolor
-              magna eget est lorem. Erat imperdiet sed euismod nisi porta.
-              Lectus vestibulum mattis ullamcorper velit.
-            </p>
+            <Flex flexDirection="column" alignItems="center" marginTop={10}>
+              <Link
+                as={ReactLink}
+                to="/"
+                onClick={onClose}
+                display="flex"
+                alignItems="center"
+                marginY={5}
+              >
+                <Icon
+                  as={IoHomeSharp}
+                  color="#ffffff"
+                  marginRight={2}
+                  boxSize={6}
+                />
+                <Text fontWeight="bold" fontSize={24}>
+                  Home
+                </Text>
+              </Link>
+
+              <Link
+                as={ReactLink}
+                to="/profile"
+                onClick={onClose}
+                display="flex"
+                alignItems="center"
+                marginY={5}
+              >
+                <Icon
+                  as={IoPersonSharp}
+                  color="#ffffff"
+                  marginRight={2}
+                  boxSize={6}
+                />
+                <Text fontWeight="bold" fontSize={24}>
+                  Profile
+                </Text>
+              </Link>
+
+              <Link
+                as={ReactLink}
+                to="/projects"
+                onClick={onClose}
+                display="flex"
+                alignItems="center"
+                marginY={5}
+              >
+                <Icon
+                  as={IoIosConstruct}
+                  color="#ffffff"
+                  marginRight={2}
+                  boxSize={6}
+                />
+                <Text fontWeight="bold" fontSize={24}>
+                  Projects
+                </Text>
+              </Link>
+            </Flex>
           </DrawerBody>
+
+          <DrawerFooter borderTopWidth="1px">
+            <Flex alignItems="center" justifyContent="center">
+              <Link
+                href="https://www.linkedin.com/in/danieleespinal/"
+                isExternal
+              >
+                <Icon as={BsLinkedin} color="#ffffff" />
+              </Link>
+
+              <Link href="https://github.com/DanCanCode" isExternal>
+                <Icon as={BsGithub} color="#ffffff" />
+              </Link>
+            </Flex>
+          </DrawerFooter>
         </DrawerContent>
       </Drawer>
     </Flex>
